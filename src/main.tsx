93502b82f07b150ef9184e1d8f6530510c1b7bd6
[dyad] <git@dyad.sh>
2026-08-06 18:31:45 +0545

    [dyad] Enabled sleek dark theme mode as default with vibrant color accents for all components and cards. - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+// Force dark mode class on document
+document.documentElement.classList.add("dark");
+
 createRoot(document.getElementById("root")!).render(<App />);
